53d6619c4c7219d08749d29d8be69eb39f9f4699
wii <rwii0433@cchntek.com>
2020-04-10 18:15:42 +0800

    test

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express=require('express');
 const path = require('path');
 const app=express();
-app.get('/*', (req, res) => {
-res.sendFile(path.join(__dirname, './index.html'));
-})
-const htmlPort = process.env.PORT || 4000;
-app.listen(htmlPort,()=>{
-    console. log(`App running on ${htmlPort}`);
-})
+
+const htmlPort = process.env.PORT || 4003;
+app.use(express.static('client'));
+app.get('*', function (req, res) {
+
+  res.sendfile(__dirname+'/index.html');
+
+});
+
+var server = app.listen(htmlPort, function (req, res) {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("app listening at", host, port)
+});
